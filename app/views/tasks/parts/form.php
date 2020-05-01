207b7c4ce3dd3815811ee69441bbba25620e8615
yakoffka <yagithub@mail.ru>
2020-05-01 13:31:56 +0300

    Add html5 form validation

--- app/views/tasks/parts/form.php
+++ app/views/tasks/parts/form.php
@@ -1,8 +1,8 @@
 <h2>Add task</h2>
 <form action="/task/create" method="post">
-    <input name="user_name" type="text" placeholder="enter your name">
-    <input name="email" type="text" placeholder="enter your email">
-    <input name="name" type="text" placeholder="enter task name">
-    <input name="description" type="text" placeholder="enter task description">
+    <input name="user_name" type="text" placeholder="enter your name" required>
+    <input name="email" pattern=".+@.+\..+" type="text" placeholder="enter your email" required>
+    <input name="name" type="text" placeholder="enter task name" required>
+    <input name="description" type="text" placeholder="enter task description" required>
     <input type="submit" value="submit">
 </form>
--- app/views/users/login.php
+++ app/views/users/login.php
@@ -1,6 +1,6 @@
 <h2>login</h2>
 <form action="/user/authentication" method="post">
-    <input name="name" type="text" placeholder="enter your name">
-    <input name="password" type="password" placeholder="enter password">
+    <input name="name" type="text" placeholder="enter your name" required>
+    <input name="password" type="password" placeholder="enter password" required>
     <input type="submit" value="go">
 </form>
